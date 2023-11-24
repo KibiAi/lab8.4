a7a55584ff1613780f451e97bbc8d3a3b0c06d51
KibiAi <vetal2004v@gmail.com>
2023-11-24 13:15:05 +0200

    Добавьте файлы проекта.

---
+++ DataPrototype.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8._4
+{
+    public abstract class DataPrototype
+    {
+        public abstract DataPrototype Clone();
+    }
+
+    public class CSVData : DataPrototype
+    {
+        public override DataPrototype Clone()
+        {
+            return (CSVData)this.MemberwiseClone();
+        }
+    }
+
+    public class XMLData : DataPrototype
+    {
+        public override DataPrototype Clone()
+        {
+            return (XMLData)this.MemberwiseClone();
+        }
+    }
+
+    public class JSONData : DataPrototype
+    {
+        public override DataPrototype Clone()
+        {
+            return (JSONData)this.MemberwiseClone();
+        }
+    }
+
+    public interface IDataAdapter
+    {
+        void Convert(DataPrototype data);
+    }
+
+    public class CSVToXMLAdapter : IDataAdapter
+    {
+        public void Convert(DataPrototype data)
+        {
+            Console.WriteLine("Конвертація CSV в XML");
+        }
+    }
+
+    public class CSVToJSONAdapter : IDataAdapter
+    {
+        public void Convert(DataPrototype data)
+        {
+            Console.WriteLine("Конвертація CSV в JSON");
+        }
+    }
+
+    public class XMLToJSONAdapter : IDataAdapter
+    {
+        public void Convert(DataPrototype data)
+        {
+            Console.WriteLine("Конвертація XML в JSON");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using lab8._4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть вихідний формат даних (CSV, XML, JSON):");
+        string sourceFormat = Console.ReadLine();
+        Console.WriteLine("Введіть цільовий формат даних (CSV, XML, JSON):");
+        string targetFormat = Console.ReadLine();
+
+        DataPrototype data;
+        switch (sourceFormat)
+        {
+            case "CSV":
+                data = new CSVData();
+                break;
+            case "XML":
+                data = new XMLData();
+                break;
+            case "JSON":
+                data = new JSONData();
+                break;
+            default:
+                Console.WriteLine("Невідомий формат даних");
+                return;
+        }
+
+        IDataAdapter adapter;
+        if (sourceFormat == "CSV" && targetFormat == "XML")
+        {
+            adapter = new CSVToXMLAdapter();
+        }
+        else if (sourceFormat == "CSV" && targetFormat == "JSON")
+        {
+            adapter = new CSVToJSONAdapter();
+        }
+        else if (sourceFormat == "XML" && targetFormat == "JSON")
+        {
+            adapter = new XMLToJSONAdapter();
+        }
+        else
+        {
+            Console.WriteLine("Неможливо конвертувати з вихідного формату в цільовий");
+            return;
+        }
+
+        adapter.Convert(data);
+    }
+}
